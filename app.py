7005670cc5a52b2c1ee9e4268846ab12466a194f
Tughan Belbek <Tughan.Belbek@t-hive.io>
2024-12-11 14:48:01 +0100

    Remove unnecessary comment about adjusting CSS styles in get_all_counts function

--- app.py
+++ app.py
@@ -387,7 +387,6 @@ def get_all_counts():
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
             <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
             <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.bootstrap5.min.css">
-            // Adjust the CSS styles
             <style>
                 .dataTables_wrapper {{
                     width: 100%;
